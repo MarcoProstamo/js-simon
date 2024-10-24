770f6b56f8c06a21e32c5491ba60843e55797aae
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-24 13:53:35 +0200

    Add → Selecting DOM Elements

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// # Selecting DOM Elements
+const numberOneEl = document.querySelector("#numberOne");
+const numberTwoEl = document.querySelector("#numberTwo");
+const numberThreeEl = document.querySelector("#numberThree");
+const numberFourEl = document.querySelector("#numberFour");
+const numberFiveEl = document.querySelector("#numberFive");
+const startGameEl = document.querySelector("#startGame");
+const submitChoicesEl = document.querySelector("#submitChoices");
