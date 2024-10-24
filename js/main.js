f89dd0330ce6d7a9021d984f779ab4b936cc22dc
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-24 23:00:48 +0200

    Feat → Added User Input Validation and Refresh Handler

--- js/main.js
+++ js/main.js
@@ -43,11 +43,11 @@ const maxNumber = 99;
 // # Game Start
 startGameEl.addEventListener("click", () => {
   // * Reset Classes
-  numberOneEl.classList.remove("text-success", "fw-bold");
-  numberTwoEl.classList.remove("text-success", "fw-bold");
-  numberThreeEl.classList.remove("text-success", "fw-bold");
-  numberFourEl.classList.remove("text-success", "fw-bold");
-  numberFiveEl.classList.remove("text-success", "fw-bold");
+  numberOneEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberTwoEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberThreeEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberFourEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberFiveEl.classList.remove("text-bg-danger", "text-bg-success");
   displayResultEl.classList.add("d-none");
   displayResultEl.classList.remove("d-block");
 
@@ -79,8 +79,16 @@ startGameEl.addEventListener("click", () => {
 });
 
 // # Submit Choices
-
 submitChoicesEl.addEventListener("click", () => {
+  // * Reset Classes
+  numberOneEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberTwoEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberThreeEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberFourEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberFiveEl.classList.remove("text-bg-danger", "text-bg-success");
+  displayResultEl.classList.add("d-none");
+  displayResultEl.classList.remove("d-block");
+
   // * Init User Numbers
   const numberOneToValidate = parseInt(numberOneEl.value.trim());
   const numberTwoToValidate = parseInt(numberTwoEl.value.trim());
@@ -96,33 +104,125 @@ submitChoicesEl.addEventListener("click", () => {
     numberFiveToValidate,
   ];
 
+  // * Validate User Input
+  let isInputValid = true;
+
+  const isInputNan = numbersToValidate.includes(NaN);
+  const isInputInRange = !numbersToValidate.every(
+    (number) => number <= 99 && number >= 1
+  );
+
+  if (isInputNan) {
+    isInputValid = false;
+    numbersToValidate.forEach((number, i) => {
+      if (isNaN(number)) {
+        switch (i) {
+          case 0:
+            numberOneEl.classList.add("text-bg-danger");
+            break;
+          case 1:
+            numberTwoEl.classList.add("text-bg-danger");
+            break;
+          case 2:
+            numberThreeEl.classList.add("text-bg-danger");
+            break;
+          case 3:
+            numberFourEl.classList.add("text-bg-danger");
+            break;
+          case 4:
+            numberFiveEl.classList.add("text-bg-danger");
+            break;
+        }
+      }
+    });
+  }
+
+  if (isInputInRange) {
+    isInputValid = false;
+    numbersToValidate.forEach((number, i) => {
+      if (number > 99 || number < 1) {
+        switch (i) {
+          case 0:
+            numberOneEl.classList.add("text-bg-danger");
+            break;
+          case 1:
+            numberTwoEl.classList.add("text-bg-danger");
+            break;
+          case 2:
+            numberThreeEl.classList.add("text-bg-danger");
+            break;
+          case 3:
+            numberFourEl.classList.add("text-bg-danger");
+            break;
+          case 4:
+            numberFiveEl.classList.add("text-bg-danger");
+            break;
+        }
+      }
+    });
+  }
+
   // * Check User Numbers
   let correctChoiceCunter = 0;
-  numbersToValidate.forEach((number, i) => {
-    if (savedArray.includes(number)) {
-      correctChoiceCunter++;
-      switch (i) {
-        case 0:
-          numberOneEl.classList.add("text-success", "fw-bold");
-          break;
-        case 1:
-          numberTwoEl.classList.add("text-success", "fw-bold");
-          break;
-        case 2:
-          numberThreeEl.classList.add("text-success", "fw-bold");
-          break;
-        case 3:
-          numberFourEl.classList.add("text-success", "fw-bold");
-          break;
-        case 4:
-          numberFiveEl.classList.add("text-success", "fw-bold");
-          break;
+  if (isInputValid) {
+    numbersToValidate.forEach((number, i) => {
+      if (savedArray.includes(number)) {
+        correctChoiceCunter++;
+        switch (i) {
+          case 0:
+            numberOneEl.classList.add("text-bg-success");
+            break;
+          case 1:
+            numberTwoEl.classList.add("text-bg-success");
+            break;
+          case 2:
+            numberThreeEl.classList.add("text-bg-success");
+            break;
+          case 3:
+            numberFourEl.classList.add("text-bg-success");
+            break;
+          case 4:
+            numberFiveEl.classList.add("text-bg-success");
+            break;
+        }
+      } else {
+        if (!numberOneEl.classList.contains("text-bg-success"))
+          numberOneEl.classList.add("text-bg-danger");
+        if (!numberTwoEl.classList.contains("text-bg-success"))
+          numberTwoEl.classList.add("text-bg-danger");
+        if (!numberThreeEl.classList.contains("text-bg-success"))
+          numberThreeEl.classList.add("text-bg-danger");
+        if (!numberFourEl.classList.contains("text-bg-success"))
+          numberFourEl.classList.add("text-bg-danger");
+        if (!numberFiveEl.classList.contains("text-bg-success"))
+          numberFiveEl.classList.add("text-bg-danger");
       }
-    }
-  });
+    });
+  } else {
+    alert("Input NON Validi: Controllo Risposte NON Effettuato");
+  }
 
   // * Output
   correctAnswersEl.innerText = `${correctChoiceCunter}`;
   displayResultEl.classList.add("d-block");
   displayResultEl.classList.remove("d-none");
+});
+
+// # Reset on Refresh
+addEventListener("beforeunload", () => {
+  // * Reset Classes
+  numberOneEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberTwoEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberThreeEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberFourEl.classList.remove("text-bg-danger", "text-bg-success");
+  numberFiveEl.classList.remove("text-bg-danger", "text-bg-success");
+  displayResultEl.classList.add("d-none");
+  displayResultEl.classList.remove("d-block");
+
+  // * Reset Values
+  numberOneEl.value = "—";
+  numberTwoEl.value = "—";
+  numberThreeEl.value = "—";
+  numberFourEl.value = "—";
+  numberFiveEl.value = "—";
 });
