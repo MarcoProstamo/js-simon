b298d9f5c7d08909ac3205d4ed5410ce91d3c907
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-24 15:03:12 +0200

    Feat → Validation and Graphic Output

--- js/main.js
+++ js/main.js
@@ -20,10 +20,19 @@ function randomIntegerNumberInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+let savedArray = [];
+function saveArray(array = [], override) {
+  if (override) savedArray = array;
+  console.log(array, savedArray);
+
+  return savedArray;
+}
+
 // # Variable Init
 const minNumber = 1;
 const maxNumber = 99;
 
+// # Game Start
 startGameEl.addEventListener("click", () => {
   // * Generate Random Numbers
   const numberOne = randomIntegerNumberInRange(minNumber, maxNumber);
@@ -32,6 +41,9 @@ startGameEl.addEventListener("click", () => {
   const numberFour = randomIntegerNumberInRange(minNumber, maxNumber);
   const numberFive = randomIntegerNumberInRange(minNumber, maxNumber);
 
+  const numbers = [numberOne, numberTwo, numberThree, numberFour, numberFive];
+  saveArray(numbers, true);
+
   // * Show Numbers in DOM
   numberOneEl.value = numberOne;
   numberTwoEl.value = numberTwo;
@@ -39,6 +51,7 @@ startGameEl.addEventListener("click", () => {
   numberFourEl.value = numberFour;
   numberFiveEl.value = numberFive;
 
+  // * Reset Values
   setTimeout(() => {
     numberOneEl.value = "—";
     numberTwoEl.value = "—";
@@ -46,4 +59,45 @@ startGameEl.addEventListener("click", () => {
     numberFourEl.value = "—";
     numberFiveEl.value = "—";
   }, 3000);
+});
+
+// # Submit Choices
+submitChoicesEl.addEventListener("click", () => {
+  const numberOneToValidate = parseInt(numberOneEl.value.trim());
+  const numberTwoToValidate = parseInt(numberTwoEl.value.trim());
+  const numberThreeToValidate = parseInt(numberThreeEl.value.trim());
+  const numberFourToValidate = parseInt(numberFourEl.value.trim());
+  const numberFiveToValidate = parseInt(numberFiveEl.value.trim());
+
+  const numbersToValidate = [
+    numberOneToValidate,
+    numberTwoToValidate,
+    numberThreeToValidate,
+    numberFourToValidate,
+    numberFiveToValidate,
+  ];
+
+  let correctChoiceCunter = 0;
+  numbersToValidate.forEach((number, i) => {
+    if (savedArray.includes(number)) {
+      correctChoiceCunter++;
+      switch (i) {
+        case 0:
+          numberOneEl.classList.add("text-success", "fw-bold");
+          break;
+        case 1:
+          numberTwoEl.classList.add("text-success", "fw-bold");
+          break;
+        case 2:
+          numberThreeEl.classList.add("text-success", "fw-bold");
+          break;
+        case 3:
+          numberFourEl.classList.add("text-success", "fw-bold");
+          break;
+        case 4:
+          numberFiveEl.classList.add("text-success", "fw-bold");
+          break;
+      }
+    }
+  });
 });
