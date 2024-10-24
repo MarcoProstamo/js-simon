733e4b09e01a12ffe383e86dd3c893771d783f43
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-24 14:00:23 +0200

    Feat → Utility Fn - Random Number

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ const numberFourEl = document.querySelector("#numberFour");
 const numberFiveEl = document.querySelector("#numberFive");
 const startGameEl = document.querySelector("#startGame");
 const submitChoicesEl = document.querySelector("#submitChoices");
+
+// # Utility Fn
+/**
+ *
+ * Returns a random integer in the specified range
+ *
+ * @param {number} min Minimum value of the range
+ * @param {number} max Maximum value of the range
+ * @returns {number}
+ */
+function randomIntegerNumberInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
